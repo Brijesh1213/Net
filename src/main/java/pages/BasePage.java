8fe0e952b454a992dd2ed1ee6a5a3efd5d3e2852
Brijesh Trivedi <brijesh.trivedi@netsweeper.com>
2023-02-05 02:17:21 -0500

    net

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,59 @@
+package pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class BasePage extends AbstractPage{
+
+	public BasePage(WebDriver driver) {
+		super(driver);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+ 
+	@Override
+	public String getTitle() {	
+		return driver.getTitle();
+	}
+
+
+	@Override
+	public WebElement getElementforPage(By locator) {
+		WebElement element = null;
+		try {
+			element = driver.findElement(locator);
+			return element;
+		} catch (Exception e) {
+			System.out.println("Some err occered while creation of element"+ locator.toString());
+		    e.printStackTrace();
+		} return element;
+	}
+
+	@Override
+	public void waitforElementPresent(By locator) {
+		try {
+			wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+		}
+		catch(Exception e) {
+			System.out.println("Exception occered while waiting for"+locator.toString());
+		}
+		
+	}
+
+	@Override
+	public void waitForPageTitle(String title) {
+		try {
+			wait.until(ExpectedConditions.titleContains(title));
+		}
+		catch(Exception e) {
+			System.out.println("Exception occered while waiting for title"+ title);
+		}
+	}
+
+	
+	
+
+}
